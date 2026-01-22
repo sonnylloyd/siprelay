1e4398893c50034fcc15fa2c839e58514111b624
sonny lloyd <echromlog@googlemail.com>
2026-01-22 09:40:10 +0000

    removed unused code

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
-import { Config } from './configurations';
-import { ConsoleLogger } from './logging';
 import { Logger } from './logging/Logger';
-import { TlsProxy } from './proxies/TlsProxy';
-import { UdpProxy } from './proxies/UdpProxy';
 import { ApiServer } from './http';
 import { ServiceWatcher } from './watchers/ServiceWatcher';
 import { ProxyInitializer } from './bootstrap/ProxyInitializer';
 import { container } from './container';
-import * as fs from 'fs';
 
 const logger = container.resolve<Logger>('logger');
-const config = container.resolve<Config>('config');
 
 // Initialize Service Watcher to dynamically update PBX records
 const serviceWatcher = container.resolve<ServiceWatcher>('serviceWatcher');
